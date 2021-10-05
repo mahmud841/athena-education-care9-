d524dc8af2e23b6ff2ac020b8e0505feb54a380e
AL MAHMUD SURUJ <mahmudsuruj14@gmail.com>
2021-10-05 14:47:39 +0800

    seventh

---
+++ src/components/NotFound/Notfound.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./Notfound.css"
+
+const Notfound = () => {
+    return (
+        <div>
+            <h2>This page is Not Found-404</h2>
+            <p>You can't find anything here </p>
+        </div>
+    );
+};
+
+export default Notfound;
