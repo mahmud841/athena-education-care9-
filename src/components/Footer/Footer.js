d9d117f1a171cc943a8666086ab3662e1d436c74
AL MAHMUD SURUJ <mahmudsuruj14@gmail.com>
2021-10-05 18:08:10 +0800

    footer complete

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,35 +4,36 @@ import './Footer.css';
 
 const Footer = () => {    
     return (
-        // -----------------    Footer    -----------------
-        <div className="Footer">
+        //*********** Footer*******************    
+        <div className="card-footer">
             <div className="container">
-            {/* -----------------    3 Column Row    ----------------- */}
+            {/****************three column ************/}
                 <div className="row">
-                    {/* -----------------    Our Goal    ----------------- */}
+
                     <div className="col-md-4 col-12">
-                        <h5 className="section-title">Our Goal</h5>
-                        <p className="pe-4">To spread the light of programming knowledge among beginner programmers within their budget</p>
+                        <h5 className="footer-title"><i class="fab fa-pagelines"></i> Target </h5>
+                        <p className="pe-4">To Fulfill everybody basic education with quality Teachers </p>
                     </div>
 
-                    {/* -----------------    Get in Touch   ----------------- */}
+                    
                     <div className="col-md-4 col-12 get-in-touch">
-                        <h5 className="section-title">Get in Touch</h5>                        
-                        <p className="mb-0 mt-3"><i className="fas fa-phone-alt"></i>+91 254-310-0794</p>
-                        <p><i className="fas fa-envelope"></i>info@programming-zone.com</p>
+                        <h5 className="footer-title"><i class="fab fa-pagelines"></i> Contact Us </h5>                        
+                        <p className="mb-0 mt-3">+88 01736712102</p>
+                        <p> athenateachers23@.com</p>
                     </div>
 
-                    {/* -----------------    Search   ----------------- */}
+ 
                     <div className="col-md-4 col-12 search">
-                        <h5 className="section-title">Serach</h5>                        
-                        <input type="text" placeholder="Write here what to search"></input>
+                        <h5 className="footer-title"><i class="fab fa-pagelines"></i>  Serach</h5>                        
+                        <input type="text" placeholder="search your favourite course"></input>
                         <button className="btn px-4">Search</button>
                     </div>
                 </div>
 
                 {/* -----------------    Copyright   ----------------- */}
                 <div>
-                    <p className="text-center mb-0 mt-4">Copyright @2021 || Programming Zone</p>
+                    <p className="text-center mb-2 mt-3">
+                        Copyright @2021 <i class="fas fa-copyright"></i> Athena Education Care</p>
                 </div>
             </div>
         </div>
