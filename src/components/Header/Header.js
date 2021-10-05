2c9ab570b41c193899c72511c7a96933ae04b5f4
AL MAHMUD SURUJ <mahmudsuruj14@gmail.com>
2021-10-05 14:40:15 +0800

    header complete

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h2 className="text-center">Welcome to Athena Coaching Center</h2>
+        </div>
+    );
+};
+
+export default Header;
