00bb7ff09246e27ecea07261bab42657276e7097
AL MAHMUD SURUJ <mahmudsuruj14@gmail.com>
2021-10-05 14:44:48 +0800

    fifth

---
+++ src/components/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./Home.css"
+
+const Home = () => {
+    return (
+        <div>
+            <h2 className="text-center text-secondary">This is one the famous coaching Centre in Bangladesh</h2>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/components/Mentor/Mentor.js
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from 'react';
+import Mentors from '../Mentors/Mentors';
+import "./Mentor.css"
+
+const Mentor = () => {
+    const [mentors, setmentors] = useState([])
+    useEffect(() => {
+        fetch("./teacher.json")
+            .then(res => res.json())
+            .then(data => setmentors(data))
+    }, [])
+    return (
+        <div >
+            <h2 className="text-center"> Mentor:{mentors.length}</h2>
+            <div className="mentors-container">
+
+                {
+                    mentors.map(mentor => <Mentors
+                        mentor={mentor}
+                        key={mentor.id}
+                    ></Mentors>)
+                }
+            </div>
+        </div>
+    );
+};
+
+export default Mentor;
